480b58ea2d069115e66478cec2dd8629b34e40ad
Marissa Fortier <marissafortier@gmail.com>
2019-10-25 19:53:53 -0400

    created click event to localstore

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-$("#momentDate").text(moment().format("LL"));
+
+// Refresh clock every min
+setInterval($("#momentDate").text(moment().format("LLL")), 60000);
+
+// store key value pair in LocalStorage
+$("button").on("click", function () {
+    var key = $(this).attr("data-info");
+    var value = $("#" + key).val();
+    localStorage.setItem(key, value);
+});
+
